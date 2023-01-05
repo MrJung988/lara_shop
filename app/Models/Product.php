d5d1136ef3bdc291c65feb04f54c7da13c41acb7
MrJung988 <karkisamir99@gmail.com>
2023-01-05 13:43:57 +0545

    7th task/ Products CRUD done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,15 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name', 'description', 'price', 'category_id', 'vendor_id'
+    ];
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VendorController;
 use Illuminate\Http\Request;
@@ -14,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('users', UserController::class);
 Route::resource('vendors', VendorController::class);
 Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
